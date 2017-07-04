45d558f6cc210b4c3dcc58e305b4259a6d52ae6f
vladyslav <vlnerodenko@gmail.com>
2017-07-04 19:09:14 +0300

    initial version

--- result_printer.rb
+++ result_printer.rb
@@ -4,7 +4,8 @@ class ResultPrinter
   end
 
   def print_res(test)
-    puts "\n Ваш результат теста (ответов да - #{test.yes_answer},ответов нет - #{test.no_answer},ответов иногда - #{test.sometimes_answer})"
+    puts "\nВаш результат теста:"
+    puts "ответов да - #{test.yes_answer},ответов нет - #{test.no_answer},ответов иногда - #{test.sometimes_answer}"
     puts @answers[test.check_result]
   end
 end
